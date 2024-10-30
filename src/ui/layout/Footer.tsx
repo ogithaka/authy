27ca18180122abfa06f6578cf2db3d1a1e40e000
ogithaka <ogithaka@gmail.com>
2024-10-30 17:48:15 +0300

    feat(ui/layout): add `Footer` component content

--- src/ui/layout/Footer.tsx
+++ src/ui/layout/Footer.tsx
@@ -1,5 +1,9 @@
 import { LayoutStyles } from '@/exports/styles';
 
 export default function Footer() {
-    return <footer className={LayoutStyles.Footer}></footer>;
+    return (
+        <footer className={LayoutStyles.Footer}>
+            <p>© {new Date().getFullYear()} Verfy. All rights reserved.</p>
+        </footer>
+    );
 }
