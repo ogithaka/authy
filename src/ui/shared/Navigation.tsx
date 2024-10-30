6fd6c9b451de4910d2970234d0220d5cf1b33a33
ogithaka <ogithaka@gmail.com>
2024-10-30 16:30:26 +0300

    feat(ui/shared): customize `Navigation` component to accept mobile navigation

--- src/ui/shared/Navigation.tsx
+++ src/ui/shared/Navigation.tsx
@@ -1,5 +1,15 @@
 import { SharedStyles } from '@/exports/styles';
 
 export default function Navigation({ children }: NavigationProps) {
-    return <nav className={SharedStyles.Navigation}>{children}</nav>;
+    return (
+        <nav className={SharedStyles.Navigation}>
+            <div className={SharedStyles.NavigationFull}>{children}</div>
+            <div className={SharedStyles.NavigationSmall}>
+                <div className={SharedStyles.NavigationSmallTrigger}></div>
+                <div className={SharedStyles.NavigationDropdown}>
+                    {children}
+                </div>
+            </div>
+        </nav>
+    );
 }
