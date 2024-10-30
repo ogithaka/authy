6a7fb2f4d78eb07aa5f59f158ad2787be86dc80f
ogithaka <ogithaka@gmail.com>
2024-10-30 12:44:18 +0300

    feat(ui/layout): create and export the `Container` component

--- src/exports/layout.ts
+++ src/exports/layout.ts
@@ -1,3 +1,4 @@
 export { default as Header } from '../ui/layout/Header';
 export { default as Main } from '../ui/layout/Main';
 export { default as Footer } from '../ui/layout/Footer';
+export { default as Container } from '../ui/layout/Container';
