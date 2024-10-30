e7bd0ac736334b59eb1ee5a7ff2dccf0b05b30e2
ogithaka <ogithaka@gmail.com>
2024-10-30 15:45:00 +0300

    style(ui/shared): style `NavigationItem` component - included `hover` styles

--- src/ui/shared/NavigationItem.tsx
+++ src/ui/shared/NavigationItem.tsx
@@ -1,5 +1,13 @@
+import { SharedStyles } from '@/exports/styles';
 import Link from 'next/link';
 
 export default function NavigationItem({ url, name }: NavigationItemProps) {
-    return <Link href={url}>{name}</Link>;
+    return (
+        <Link
+            href={url}
+            className={SharedStyles.NavigationItem}
+        >
+            {name}
+        </Link>
+    );
 }
