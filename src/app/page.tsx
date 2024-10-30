8340f104c48ca95330f5e5b99582712d7837eba8
ogithaka <ogithaka@gmail.com>
2024-10-30 15:33:41 +0300

    feat(app/page): mount `NavigationItems` - mounted `NavigationItem` components to serve as header navigation i.e. landing and docs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import { Container, Footer, Header, Main } from '@/exports/layout';
+import { NavigationItem } from '@/exports/shared';
 
 export default function Landing() {
     return (
         <>
-            <Header />
+            <Header>
+                <NavigationItem
+                    url='/'
+                    name='Landing'
+                />
+                <NavigationItem
+                    url='/docs'
+                    name='Docs'
+                />
+            </Header>
             <Container>
                 <Main />
                 <Footer />
