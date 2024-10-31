98792e6e4bedca61b4edd5dd3fdd8f4c4e5b3afa
ogithaka <ogithaka@gmail.com>
2024-10-31 10:36:55 +0300

    feat(ui/views): add `Hero` section content

--- src/ui/views/Hero.tsx
+++ src/ui/views/Hero.tsx
@@ -1,15 +1,49 @@
+import Image from 'next/image';
+
 export default function Hero() {
     return (
         <section id='hero'>
             <div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div>
+                    <Image
+                        src={'/images/landing/hero-1.jpg'}
+                        alt='Hero Image 1'
+                        width={700}
+                        height={500}
+                    />
+                </div>
+                <div>
+                    <p>Listeners</p>
+                    <h1>340k+</h1>
+                </div>
+                <div>
+                    <p>Streams</p>
+                    <h1>500k+</h1>
+                </div>
             </div>
             <div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div>
+                    <h1>Unlock Your Potential</h1>
+                    <p>With our self improovement podcast</p>
+                </div>
+                <div>
+                    <Image
+                        src={'/images/landing/hero-2.jpg'}
+                        alt='Hero Image 2'
+                        width={100}
+                        height={100}
+                    />
+                    <h1>Episode 205 - Mastering Mindfulness</h1>
+                </div>
+                <div>
+                    <p>Listen on</p>
+                    <Image
+                        src={'/icons/github.svg'}
+                        alt='Github Icon'
+                        width={40}
+                        height={40}
+                    ></Image>
+                </div>
             </div>
         </section>
     );
